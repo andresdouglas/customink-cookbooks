273339213451dd47bd73b86876aba0c5f1225142
Nathen Harvey <nharvey@customink.com>
2010-12-21 07:21:15 -0500

    basic mongodb cookbook template

---
+++ mongodb/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "CustomInk, LLC"
+maintainer_email "nharvey@customink.com"
+license          "All rights reserved"
+description      "Installs/Configures mongodb"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ mongodb/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: mongodb
+# Recipe:: default
+#
+# Copyright 2010, CustomInk, LLC
+#
+# All rights reserved - Do Not Redistribute
+#
