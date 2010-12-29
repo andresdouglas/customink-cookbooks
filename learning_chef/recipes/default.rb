5857eeba14e4451b0cf7b88102e8d87cc621ad4c
Nathen Harvey <nharvey@customink.com>
2010-12-29 10:24:14 -0500

    use node.override for the hashes

--- learning_chef/recipes/default.rb
+++ learning_chef/recipes/default.rb
@@ -20,10 +20,13 @@ node.set.my_integer = 56
 node.my_array.push("an additional item, added by the recipe file")
 
 # adding to a hash is easy enough
-node.my_hash = node.my_hash.merge({"something" => "else"})
+node.override.my_hash = node.my_hash.merge({"something" => "else"})
 
 # but merging like this yields unexpected results
 node.my_hash.merge!({"something" => "else"})
+
+node.override.my_hash =  node.my_hash.merge({"something1" => "else1"})
+
 
 template "/tmp/attribute.txt" do
   mode "0777"
