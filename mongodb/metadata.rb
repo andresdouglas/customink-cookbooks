665dab1fca6f0cf6ebc1108e8581db8806d70324
Nathen Harvey <nharvey@customink.com>
2011-09-30 22:32:36 -0400

    list the proper license, Apache 2.0

--- mongodb/metadata.rb
+++ mongodb/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "CustomInk, LLC"
 maintainer_email "nharvey@customink.com"
-license          "All rights reserved"
+license           "Apache 2.0"
 description      "Installs/Configures mongodb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
